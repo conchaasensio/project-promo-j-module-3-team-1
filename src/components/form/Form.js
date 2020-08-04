6179c747e8969044b201e4636626c553d67fe09d
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-04 18:26:56 +0200

    add local storage to Card

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "../../stylesheets/form/_form.scss";
-import "../../stylesheets/preview/_preview.scss";
-import "../../stylesheets/common/_page.scss";
-import Design from "./Design";
-import Fill from "./Fill";
-import Share from "./Share";
+import React from 'react';
+import '../../stylesheets/form/_form.scss';
+import '../../stylesheets/preview/_preview.scss';
+import '../../stylesheets/common/_page.scss';
+import Design from './Design';
+import Fill from './Fill';
+import Share from './Share';
 
 const Form = (props) => {
   return (
