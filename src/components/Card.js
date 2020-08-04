6d530d94c77db3d49b0ea992dd020b46b75f7d11
sandratapia <sandritant62@gmail.com>
2020-08-04 18:59:05 +0200

    reset function

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,49 @@
-import React, { useState } from 'react';
-import Header from './header-footer/Header';
-import Form from './form/Form';
-import Footer from './header-footer/Footer';
-import Preview from './preview/Preview';
-import '../stylesheets/common/_app.scss';
+import React, { useState } from "react";
+import Header from "./header-footer/Header";
+import Form from "./form/Form";
+import Footer from "./header-footer/Footer";
+import Preview from "./preview/Preview";
+import "../stylesheets/common/_app.scss";
 
 function Card() {
-  const [name, setName] = useState('');
-  const [job, setJob] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [linkedin, setLinkedin] = useState('');
-  const [github, setGithub] = useState('');
-  const [photo, setPhoto] = useState('');
-  const [palette, setPalette] = useState('1');
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [github, setGithub] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [palette, setPalette] = useState("1");
 
   const handleInput = (data) => {
-    if (data.inputKey === 'name') {
+    if (data.inputKey === "name") {
       setName(data.inputValue);
-    } else if (data.inputKey === 'job') {
+    } else if (data.inputKey === "job") {
       setJob(data.inputValue);
-    } else if (data.inputKey === 'email') {
+    } else if (data.inputKey === "email") {
       setEmail(data.inputValue);
-    } else if (data.inputKey === 'phone') {
+    } else if (data.inputKey === "phone") {
       setPhone(data.inputValue);
-    } else if (data.inputKey === 'linkedin') {
+    } else if (data.inputKey === "linkedin") {
       setLinkedin(data.inputValue);
-    } else if (data.inputKey === 'github') {
+    } else if (data.inputKey === "github") {
       setGithub(data.inputValue);
-    } else if (data.inputKey === 'palette') {
+    } else if (data.inputKey === "palette") {
       setPalette(data.inputValue);
-    } else if (data.inputKey === 'photo') {
+    } else if (data.inputKey === "photo") {
       setPhoto(data.inputValue);
     }
+  };
+
+  const resetCard = () => {
+    setName(Card.defaultProps.defaultName);
+    setJob(Card.defaultProps.defaultJob);
+    setEmail("");
+    setPhone("");
+    setLinkedin("");
+    setGithub("");
+    setPalette(Card.defaultProps.defaultPalette);
+    setPhoto("");
   };
 
   return (
@@ -49,6 +60,7 @@ function Card() {
             github={github}
             palette={palette}
             photo={photo}
+            resetCard={resetCard}
           />
 
           <Form
@@ -69,4 +81,9 @@ function Card() {
   );
 }
 
+Card.defaultProps = {
+  defaultName: "Jeanne Baret",
+  defaultJob: "Botánica",
+  defaultPalette: "1",
+};
 export default Card;
--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import '../../stylesheets/form/_form.scss';
-import '../../stylesheets/preview/_preview.scss';
-import '../../stylesheets/common/_page.scss';
-import '../../stylesheets/preview/_image_preview.scss';
-import CardPreview from './CardPreview';
-import ButtonReset from './Reset';
+import React from "react";
+import "../../stylesheets/form/_form.scss";
+import "../../stylesheets/preview/_preview.scss";
+import "../../stylesheets/common/_page.scss";
+import "../../stylesheets/preview/_image_preview.scss";
+import CardPreview from "./CardPreview";
+import ButtonReset from "./Reset";
 
 const Preview = (props) => {
   return (
     <section className="app__preview">
       <div className="button-container">
-        <ButtonReset />
+        <ButtonReset resetCard={props.resetCard} />
       </div>
       <CardPreview
         name={props.name}
